f576b58c0e8768905b80b12565dc1dbd43d8a50b
fuxiang <20948660@qq.com>
2013-12-20 10:28:17 +0800

    添加DAL程序集信息

--- BoonieBear.DockUnit.DAL/Properties/AssemblyInfo.cs
+++ BoonieBear.DockUnit.DAL/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("BoonieBear.DockUnit.DAL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DataBase Access Lib")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("IACAS")]
 [assembly: AssemblyProduct("BoonieBear.DockUnit.DAL")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
