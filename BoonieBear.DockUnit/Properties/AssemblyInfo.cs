c238e9e7df1c03be2a06a2301667ca5a32fba89b
fuxiang <20948660@qq.com>
2014-02-03 16:38:59 +0800

    完善了二级菜单的动画效果，实现了第一个菜单的操作

--- BoonieBear.DockUnit/Properties/AssemblyInfo.cs
+++ BoonieBear.DockUnit/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("BoonieBear.DockUnit")]
+[assembly: AssemblyTitle("水声通信甲板单元显控程序")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BoonieBear.DockUnit")]
+[assembly: AssemblyProduct("DockUnit")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
